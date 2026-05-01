e858820298b59357b265ade2291f778541539691
Mohamed bahaa <mohamedbahaaman@gmail.com>
2026-05-01 19:03:25 +0300

    img

--- src/app/_components/Slider.tsx
+++ src/app/_components/Slider.tsx
@@ -2,7 +2,9 @@
 import React from 'react'
 import { Navigation, Pagination} from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 
 interface MySliderPropsType {
