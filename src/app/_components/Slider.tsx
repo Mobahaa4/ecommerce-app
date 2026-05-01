829f9f021f03a1bec9421fa7ab5f6ee85ee9d1de
Mohamed bahaa <mohamedbahaaman@gmail.com>
2026-05-01 18:58:53 +0300

    img

--- src/app/_components/Slider.tsx
+++ src/app/_components/Slider.tsx
@@ -1,8 +1,10 @@
 "use client"
-import Image from 'next/image';
 import React from 'react'
 import { Navigation, Pagination} from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 
 interface MySliderPropsType {
@@ -23,7 +25,7 @@ export default function Slider({listOfImages, spaceBetween = 100, slidesPerView 
             {listOfImages.map( (image,index) => 
                 <SwiperSlide key={index}> 
                     <div className='w-full h-100 relative'>
-                        <Image width={200} height={200}  src={image} className='w-full h-100 object-cover' alt="Home Image" /> 
+                        <img src={image} className='w-full h-100 object-cover' alt="Home Image" /> 
                         <div className='absolute inset-0 bg-emerald-400/50'></div>
                     </div>
                 </SwiperSlide> 
