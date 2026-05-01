01cf3dd3fbe45b8aba689bfa6a248497909b16a9
Mohamed bahaa <mohamedbahaaman@gmail.com>
2026-05-01 18:59:56 +0300

    img fix

--- src/app/_components/Slider.tsx
+++ src/app/_components/Slider.tsx
@@ -2,9 +2,7 @@
 import React from 'react'
 import { Navigation, Pagination} from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+
 
 
 interface MySliderPropsType {
